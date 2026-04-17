85b4c064a1709d0e566db58bef5698a3b8bb52d6
“Matt2” <a20222079@pucp.edu.pe>
2026-04-16 19:55:04 -0500

    develop

---
+++ lab2/src/main/java/com/example/lab2/entity/Mascota.java
@@ -0,0 +1,97 @@
+package com.example.lab2.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "mascota")
+public class Mascota {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private int id;
+
+    @Column(name= "nombre")
+    private String nombre;
+
+    @Column(name= "especie")
+    private String especie;
+
+    @Column(name= "raza")
+    private String raza;
+
+    @Column(name= "edad")
+    private int edad;
+
+    @Column(name= "nombre_dueno")
+    private String nombreDueno;
+
+    @Column(name= "telefono")
+    private int telefono;
+
+    @Column(name= "estado")
+    private int estado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombreDueno() {
+        return nombreDueno;
+    }
+
+    public void setNombreDueno(String nombreDueno) {
+        this.nombreDueno = nombreDueno;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+}
+
